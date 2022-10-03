04f340dc5b41deaccf2bb0b277513af0d142f9ed
seohyun0221 <seohyun0221@gmail.com>
2022-10-03 15:48:48 +0900

    입문 수업 완료

---
+++ Study/Spring_week1/Seohyun/hello.spring/src/main/java/hello/hello/spring/SpringConfig.java
@@ -0,0 +1,39 @@
+package hello.hello.spring;
+
+import hello.hello.spring.aop.TimeTraceAop;
+import hello.hello.spring.repository.*;
+import hello.hello.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Time;
+
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+
+        return new MemberService(memberRepository);
+    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
+
+//    @Bean
+//   public MemberRepository memberRepository(){
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new MemoryMemberRepository();
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        //return new JpaMemberRepository(em);
+//
+//    }
+}
